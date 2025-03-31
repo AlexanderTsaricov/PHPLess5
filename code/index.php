5e6b5c0d3e2f7641ecbcd2a5b24be82b70fd755b
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-03-31 19:02:30 +0500

    lesson 8: add looking RAM

--- code/index.php
+++ code/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$memory_start = memory_get_usage();
+
 use Geekbrains\Application1\Application\Render;
 
 
@@ -15,3 +17,7 @@ try {
     $render = new Render();
     echo $render->renderExeptionPage($e);
 }
+
+$memory_end = memory_get_usage();
+
+echo "<h4>Потреблено " . ($memory_end - $memory_start)/1024/1024 . " Мбайт памяти</h4>";
